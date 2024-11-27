e655d792813082a70aee5ef2822ef95486bf341c
TalGrinberg20 <talgrinberg20@gmail.com>
2024-11-27 15:16:04 +0200

    test

--- Algebra.java
+++ Algebra.java
@@ -6,35 +6,50 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+		System.out.println(times(-2, -3));
+		// System.out.println(plus(-3,-3));   
+		// System.out.println(plus(2,3));   // 2 + 3
+	    // System.out.println(minus(7,2));  // 7 - 2
+   		// System.out.println(minus(2,7));  // 2 - 7
+ 		// System.out.println(times(3,4));  // 3 * 4
+   		// System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
+   		// System.out.println(pow(5,3));      // 5^3
+   		// System.out.println(pow(3,5));      // 3^5
+   		// System.out.println(div(12,3));   // 12 / 3    
+   		// System.out.println(div(5,5));    // 5 / 5  
+   		// System.out.println(div(25,7));   // 25 / 7
+   		// System.out.println(mod(25,7));   // 25 % 7
+   		// System.out.println(mod(120,6));  // 120 % 6    
+   		// System.out.println(sqrt(36));
+		// System.out.println(sqrt(263169));
+   		// System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
+		if (x1<0 && x2<0) {
+			for (int i = x2; i < 0; i++) {
+			x1--;
+		}
+		} else {
 		for (int i = 0; i < x2; i++) {
 			x1++;
+		}
 		}
 		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
+		if (x1<0 && x2<0) {
+		for (int i = x2; i < 0; i++) {
+			x1++;
+		}	
+		} 
+		else {
 		for (int i = 0; i < x2; i++) {
 			x1--;
+		}
 		}
 		return x1;
 	}
@@ -42,10 +57,19 @@ public class Algebra {
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
 		int result = 0;
-		for (int i = 0; i < x2; i++) {
-			result = plus(result, x1);
+		if (x1 <0 ^ x2<0) {
+			for (int i = 0; i < x2; i++) {
+				result = plus(x1,result);
 		}
 		return result;
+
+		} 
+		else {
+			for (int i = 0; i < myABS(x2); i++) {
+				result = plus(myABS(result),myABS(x1));
+			}
+		return result;
+		}
 	}
 
 	// Returns x^n (for n >= 0)
