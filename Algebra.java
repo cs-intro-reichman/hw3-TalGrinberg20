e00c2ad9a6f1b7f8e13d062d254b2ae1701e0cc8
TalGrinberg20 <talgrinberg20@gmail.com>
2024-11-27 12:28:09 +0200

    test

--- Algebra.java
+++ Algebra.java
@@ -19,49 +19,74 @@ public class Algebra {
    		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6    
    		System.out.println(sqrt(36));
-		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
+		// System.out.println(sqrt(263169));
+   		// System.out.println(sqrt(76123));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++) {
+			x1++;
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++) {
+			x1--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		for (int i = 0; i < x2; i++) {
+			result = plus(result, x1);
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		for (int i = 0; i < n; i++) {
+			result = times(result, x);
+		}
+		return result;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		int i = 0;
+		while (result >= x2) {
+			result = minus(result, x2);
+			 i++;
+		}
+		return i;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+	int result = x1;
+		int i = 0;
+		while (result >= x2) {
+			result = minus(result, x2);
+			 i++;
+		}
+		return result;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		// double epsilon = 0.01;
+		// int g = x / 2;
+		// //cant use abs?
+		// while (Math.abs(g * g - x) > epsilon) {
+		// 	g = g - (g * g - x) / (2 * g);
+		// }		
+		// return g;
 	}	  	  
 }
